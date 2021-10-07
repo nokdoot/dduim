ad6d308c8b1063557875fb75917fb6b9ac392411
nokdoot <nokdoot@naver.com>
2021-10-08 07:03:56 +0900

    Initial Commit

---
+++ dduim/dduimApp.swift
@@ -0,0 +1,17 @@
+//
+//  dduimApp.swift
+//  dduim
+//
+//  Created by nokdoot on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct dduimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
